a04289799347b1462ddbc1cfacd7efbbba9dcede
Juan <juan@Juans-MBP.home>
2023-03-26 23:48:12 +0200

    feat: First non-scheduled version of charging

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,51 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import mu.KotlinLogging
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val invoiceService: InvoiceService,
+    private val customerService: CustomerService
 ) {
 // TODO - Add code e.g. here
+
+    private val logger = KotlinLogging.logger {}
+    private val maxNumberOfRetries = 3
+
+    fun payInvoices() {
+        // fetch only pending invoices so not to check each invoice's status before trying to charge
+        invoiceService.fetchPendingInvoices().forEach { invoice -> payInvoice(invoice, invoice.customerId) }
+        logger.info { "Fetching all pending invoices" }
+    }
+
+    fun payInvoice(invoice: Invoice, customerId: Int) {
+        val customer = customerService.fetch(customerId)
+        val invoiceId = invoice.id
+
+        if (invoice.amount.currency != customer.currency) {
+            logger.error { "Invoice '$invoiceId' currency is different from customer '$customerId' currency" }
+            throw CurrencyMismatchException(invoiceId, customerId)
+        }
+
+        // retry mechanism in case PaymentProvider external service can't process charge initially
+        var charged = false
+        var retries = 0
+        while (!charged && retries < maxNumberOfRetries) {
+            retries++
+            try {
+                charged = paymentProvider.charge(invoice)
+                logger.info { "Invoice '$invoiceId' has been paid" }
+            } catch (e: NetworkException) {
+                logger.error { "A network error happened; payment '$invoiceId' could not be completed" }
+            }
+        }
+    }
 }
---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -0,0 +1,64 @@
+package io.pleo.antaeus.core.services
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
+
+class BillingServiceTest {
+
+    private val paymentProvider = mockk<PaymentProvider>(relaxed = true)
+    private val invoiceService = mockk<InvoiceService>(relaxed = true)
+    private val customerService = mockk<CustomerService> {}
+    private val sut = BillingService(paymentProvider, invoiceService, customerService)
+
+    @Test
+    fun payInvoices_whenRetrievingAllInvoices_shouldFetchAllInvoices() {
+        // given
+        every { invoiceService.fetchPendingInvoices() } returns listOf()
+
+        // when
+        sut.payInvoices()
+
+        // then
+        verify { invoiceService.fetchPendingInvoices() }
+    }
+
+    @Test
+    fun payInvoice_whenRetrievingCustomer_shouldFetchCustomer() {
+        // given
+        val customerId = 1
+        val currency = Currency.EUR
+        val amount = Money(BigDecimal(10.02), currency)
+        val customer = Customer(customerId, currency)
+        val invoice = Invoice(2, customerId, amount, InvoiceStatus.PAID)
+        every { customerService.fetch(customerId ) } returns customer
+
+        // when
+        sut.payInvoice(invoice, customerId)
+
+        // then
+        verify { customerService.fetch(customerId) }
+    }
+
+    @Test
+    fun payInvoice_whenInvoiceCurrencyIsNotTheSameAsCustomer_shouldThrowCurrencyMismatchException() {
+        // given
+        val customerId = 1
+        val amount = Money(BigDecimal(10.02), Currency.EUR)
+        val invoice = Invoice(2, customerId, amount, InvoiceStatus.PAID)
+        val customer = Customer(customerId, Currency.DKK)
+        every { customerService.fetch(customerId ) } returns customer
+
+        // when/then
+        assertThrows<CurrencyMismatchException> {
+            sut.payInvoice(invoice, customerId)
+        }
+    }
+}
